44f91e62337034033a537eddae7b72cee0761148
Sunghyun Yoo <optid77@gmail.com>
2025-03-09 16:03:19 +0900

    Export DataSheetGrid from components/

--- src/components/datasheet-grid/datasheet-grid.tsx
+++ src/components/datasheet-grid/datasheet-grid.tsx
@@ -1,4 +1,4 @@
 import "react-datasheet-grid/dist/style.css";
 import "./custom-style.css";
 
-export { DynamicDataSheetGrid } from "react-datasheet-grid";
+export { DynamicDataSheetGrid, DataSheetGrid } from "react-datasheet-grid";
